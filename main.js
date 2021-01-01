7720e31813e1416aaa857089b1490d82aca055d8
DrewScatterday <drewscatterday@gmail.com>
2021-01-01 14:44:49 -0500

    remove writing to file and used a buffer

--- main.js
+++ main.js
@@ -2,8 +2,9 @@ var AWS = require('aws-sdk');
 const Discord = require('discord.js'); 
 const fs = require('fs');
 var auth = require('./auth.json');
+const Stream = require('stream');
 
-var m_currentPlaying = "";
+
 const client = new Discord.Client();
 const prefix = '$'; 
 const Polly = new AWS.Polly({
@@ -22,54 +23,36 @@ function getTTS(msg, text) {
 
     console.log("processing command:" + text);
 
-    var fileName = 'sounds/' + msg.id + '.mp3';
     Polly.synthesizeSpeech(params, (err, data) => {
         if (err) {
             console.log("Error getting from polly");
             return err;
         }
         if (data.AudioStream instanceof Buffer) {
-            fs.writeFile(fileName, data.AudioStream, function (fsErr) {
-                if (fsErr) {
-                    console.log("Error writing " + fileName);
-                    return fsErr;
-                } else {
-                    console.log(fileName + ' written sucessfully');
-                }
-            })
+            playSound(msg, data);
         }
     });
-
-    return fileName;
 }
 
-function deleteFile(fileName) {
-    fs.unlinkSync("./" + fileName, function(err){
-        if (err) {
-            console.log("failed to delete " + fileName);
-            console.log(err);
-            return;
-        } 
-        console.log(fileName + ' successfully deleted');                                     
-    });
-}
-
-function playSound(msg, fileName) {
+function playSound(msg, soundData) {
     var voiceChannel = msg.member.voice.channel;
     
     voiceChannel.join()
     .then(connection => {
-        m_currentPlaying = fileName; 
-        const dispatcher = connection.play(fileName);
+        var bufferStream = new Stream.PassThrough();
+        bufferStream.end(soundData.AudioStream);
+        const dispatcher = connection.play(bufferStream);
         dispatcher.on("finish", end => {
             voiceChannel.leave();
-            deleteFile(fileName);
+            //deleteFile(fileName);
             m_currentPlaying = "";
         });
     })
     
-    // TO DO: Change this to let the bot stay in the voice channel and listen for commands: 
     
+   
+    // TO DO: Change this to let the bot stay in the voice channel and listen for commands: 
+
     //if (!msg.guild.me.voice.channel) {
     // else { // else already in a voice chat 
     //     m_currentPlaying = fileName;
@@ -97,9 +80,7 @@ client.on('message', async msg => {
         if (msg.member.voice.channel) {
             var sentence = args.join(" ");
             try {
-                var file = getTTS(msg, sentence);
-                console.log(file);
-                playSound(msg, file);
+                getTTS(msg, sentence);
             }
             catch(err) {
                 msg.reply("There was an error with that command, I'm sorry");
@@ -114,11 +95,8 @@ client.on('message', async msg => {
     else if (command === 'stop') {
         if (msg.member.voice.channel) 
         {
-            console.log(m_currentPlaying);
             msg.guild.voice.connection.dispatcher.destroy();
             msg.guild.voice.connection.disconnect();
-            deleteFile(m_currentPlaying);
-            m_currentPlaying = "";
         }
     }
 });
